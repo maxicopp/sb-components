f32818e740c90ddf96feab021042fcb08540ca5d
Maximiliano Cóppola <maxicopp@outlook.com>
2022-08-05 12:31:14 -0300

    Desplegar storybook a servidores

---
+++ src/components/MyLabel.tsx
@@ -0,0 +1,32 @@
+import './mylabel.css';
+
+export interface MyLabelProps {
+    /**
+     * Este es el mensaje a mostrar en la etiqueta
+     */
+    label: string;
+    /**
+     * Este es el tamaño de la etiqueta
+     */
+    size: 'normal' | 'h1' | 'h2' | 'h3';
+    /**
+     * Colores básicos del botón
+     */
+    color?: 'primary' | 'secondary' | 'tertiary';
+    /**
+     * Si quiere todo capitalizado
+     */
+    allCaps?: boolean;
+    /**
+     * Color personalizado de la fuente
+     */
+    fontColor?: string;
+}
+
+export const MyLabel = ({ label = 'No Label', size = 'normal', color = 'primary', allCaps = false, fontColor }: MyLabelProps) => {
+    return (
+        <span className={`label ${size} text-${color}`} style={{ color: fontColor }}>
+            {allCaps ? label.toUpperCase() : label}
+        </span>
+    )
+}
